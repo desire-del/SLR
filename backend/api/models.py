b2c6101f7ec212413f73e46924f8620505d0b565
desire SOUSSOU <soussoudesire2019@gmail.com>
2024-11-30 13:57:16 +0100

    send and receive translation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
-class Video(models.Model):
+class VideoTranslation(models.Model):
     video = models.FileField(upload_to='videos/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-
+    translated_text = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
-        return self.title
+        return f'Translation for {self.language} - {self.created_at}'
